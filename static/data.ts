4b5d93b21fd3b1c092b62534c783eb9903902bde
黄强 <hq170462.jx@chinatelecom.cn>
2023-06-16 23:41:44 +0800

    init

---
+++ static/data.ts
@@ -0,0 +1,34 @@
+const dataList = [
+    {
+        "name": "搞怪香肠嘴",
+        "star": 59,
+        "url": "static/item_00-tiny.jpg"
+    },
+    {
+        "name": "库洛米",
+        "star": 49,
+        "url": "static/item_01-tiny.jpg"
+    },
+    {
+        "name": "安妮宝贝",
+        "star": 59,
+        "url": "static/item_02-tiny.jpg"
+    },
+    {
+        "name": "福袋兔",
+        "star": 38,
+        "url": "static/item_03-tiny.jpg"
+    },
+    {
+        "name": "风车发夹",
+        "star": 15,
+        "url": "static/item_04-tiny.jpg"
+    },
+    {
+        "name": "卖仔仔的娃娃",
+        "star": 99,
+        "url": "static/item_05-tiny.jpg"
+    }
+];
+
+export default dataList;
